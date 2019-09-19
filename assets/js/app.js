ec3a2d2041a8716438f0a2407d4d3a43bbe995fa
Your Name <you@example.com>
2019-09-19 13:36:06 +0700

    remove one item

--- assets/js/app.js
+++ assets/js/app.js
@@ -157,7 +157,12 @@ class UI {
         })
         // cart functionality
         cartContent.addEventListener('click', event => {
-            
+            if (event.target.classList.contains('remove-item')) {
+                let removeItem = event.target
+                let id = removeItem.dataset.id
+                cartContent.removeChild(removeItem.parentElement.parentElement)
+                this.removeItem(id)
+            }
         })
     }
 
